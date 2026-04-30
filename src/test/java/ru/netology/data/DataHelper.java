8e568cabfbd39b56c83899513e0eec3382ad7905
Svetlana Bogdan <123helga17@gmail.com>
2026-04-30 21:38:56 +0300

    fix teacher comments: lombok + info flag

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,5 +1,7 @@
 package ru.netology.data;
 
+import lombok.Value;
+
 public class DataHelper {
 
     private DataHelper() {
@@ -21,51 +23,20 @@ public class DataHelper {
         return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
 
+    @Value
     public static class AuthInfo {
-        private final String login;
-        private final String password;
-
-        public AuthInfo(String login, String password) {
-            this.login = login;
-            this.password = password;
-        }
-
-        public String getLogin() {
-            return login;
-        }
-
-        public String getPassword() {
-            return password;
-        }
+        String login;
+        String password;
     }
 
+    @Value
     public static class VerificationCode {
-        private final String code;
-
-        public VerificationCode(String code) {
-            this.code = code;
-        }
-
-        public String getCode() {
-            return code;
-        }
+        String code;
     }
 
+    @Value
     public static class CardInfo {
-        private final String number;
-        private final String testId;
-
-        public CardInfo(String number, String testId) {
-            this.number = number;
-            this.testId = testId;
-        }
-
-        public String getNumber() {
-            return number;
-        }
-
-        public String getTestId() {
-            return testId;
-        }
+        String number;
+        String testId;
     }
 }
